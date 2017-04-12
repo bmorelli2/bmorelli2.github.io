faf45d7ff17f920ee8e0945f44384ab21c37c177
bmorelli2 <bellamorelli2@gmail.com>
2017-04-12 23:16:21 +1200

    Fixed instagram not working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-var token = '696845800.975527c.47f7440dcb1e4a8fbcee05a1dbe03612';
-
 $(document).ready(function(){
   var anchor = $("#portAttach");
 
@@ -47,31 +45,31 @@ $(document).ready(function(){
         $("body").removeClass("body-disable");
     }
   });
+  getInsta();
+});
 
-
-
-  var token = '696845800.975527c.47f7440dcb1e4a8fbcee05a1dbe03612',
-      userid = 696845800,
-      num_photos = 10;
+function getInsta(){
+  var a = '696845800.975527c.47f7440dcb1e4a8fbcee05a1dbe03612',
+      b = 696845800,
+      c = 12;
 
   $.ajax({
-  	url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
-  	dataType: 'jsonp',
-  	type: 'GET',
-  	data: {access_token: token, count: num_photos},
-  	success: function(data){
-        for( x in data.data ){
+    url: 'https://api.instagram.com/v1/users/' + b + '/media/recent',
+    dataType: 'jsonp',
+    type: 'GET',
+    data: {access_token: a, count: c},
+    success: function(json){
+
+        for( x in json.data ){
+
         $('#instaAttach').append( '<div class="p-card">' +
-                                  '<a href="'+ data.data[x].link + '" title="Instagram.com/bellimorelli">' +
+                                  '<a href="' + json.data[x].link + '" title="Instagram.com/bellimorelli">' +
                                   '<div class="p-card-img">' +
-                                  '<img src="' + data.data[x].images.low_resolution.url + '">' +
+                                  '<img src="' + json.data[x].images.low_resolution.url + '">' +
                                   '</div>' +
                                   '</a>' +
                                   '</div>');
-  		}
-  	},
-  	error: function(data){
-  		console.log(data); // send the error notifications to console
-  	}
+      }
+    }
   });
-});
+}
