4275029120708325fd35559df02853505fac4ca5
bmorelli2 <bellamorelli2@gmail.com>
2017-04-12 22:48:35 +1200

    Revised work

--- App/src/app.js
+++ App/src/app.js
@@ -1,1 +1,77 @@
 var token = '696845800.975527c.47f7440dcb1e4a8fbcee05a1dbe03612';
+
+$(document).ready(function(){
+  var anchor = $("#portAttach");
+
+  $(portfolioArray).each(function(index){
+    var htmlCard = "<div class='p-card' data-id=" + index + ">" +
+                    "<div class='p-card-imgCont'>" +
+                    "<img src='" + this.src + "' alt='" + this.title + "' />" +
+                    "</div>" +
+                    "<div class='p-card-header'>" +
+                    this.title +
+                    "</div>"+
+                    "</div>";
+    $(anchor).append(htmlCard);
+  });
+
+  $(".p-card").on('click', function(){
+    var cardIndex = $(this).attr('data-id');
+    var prompt = "<div class='disable'>" +
+                "<div class='p-window col-s-8 col-m-7 col-l-6'>" +
+                "<div class='p-window-header'>" +
+                portfolioArray[cardIndex].title +
+                "</div>" +
+                "<div class='p-window-imgCont'>" +
+                "<img src='" + portfolioArray[cardIndex].src + "' alt='" + portfolioArray[cardIndex].title + "' />" +
+                "</div>" +
+                "<div class='p-window-disc'>" +
+                portfolioArray[cardIndex].disc +
+                "</div>" +
+                "</div>" +
+                "</div>";
+
+    $("body").addClass("body-disable");
+    $("body").before(prompt);
+
+    $(".disable").on('click', function(){
+      restoreView();
+    });
+
+    $(document).on('scroll', function(){
+      restoreView();
+    });
+
+    function restoreView(x){
+        $(".disable").remove();
+        $("body").removeClass("body-disable");
+    }
+  });
+
+
+
+  var token = '696845800.975527c.47f7440dcb1e4a8fbcee05a1dbe03612',
+      userid = 696845800,
+      num_photos = 10;
+
+  $.ajax({
+  	url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+  	dataType: 'jsonp',
+  	type: 'GET',
+  	data: {access_token: token, count: num_photos},
+  	success: function(data){
+        for( x in data.data ){
+        $('#instaAttach').append( '<div class="p-card">' +
+                                  '<a href="'+ data.data[x].link + '" title="Instagram.com/bellimorelli">' +
+                                  '<div class="p-card-img">' +
+                                  '<img src="' + data.data[x].images.low_resolution.url + '">' +
+                                  '</div>' +
+                                  '</a>' +
+                                  '</div>');
+  		}
+  	},
+  	error: function(data){
+  		console.log(data); // send the error notifications to console
+  	}
+  });
+});
---
+++ App/src/portfolio.js
@@ -0,0 +1,63 @@
+var portfolioArray = [
+  {
+    "title":"Example title",
+    "src":"assets/04-portfolio-image-001.jpg",
+    "disc":"Here is an image that i made."
+  }, {
+    "title":"Example title 2",
+    "src":"assets/04-portfolio-image-002.jpg",
+    "disc":"I also made this image."
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-003.jpg",
+    "disc":"Here is another image that i made."
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-004.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-005.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-006.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-007.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-008.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-009.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-010.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-011.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-012.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-013.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-014.jpg",
+    "disc":""
+  }, {
+    "title":"",
+    "src":"assets/04-portfolio-image-015.jpg",
+    "disc":""
+  }
+];
